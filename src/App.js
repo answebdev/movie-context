7acc7dde938689368db39e6ca98a4dd629e34bc9
ANS <36783010+answebdev@users.noreply.github.com>
2021-02-26 22:12:21 -0800

    add Nav component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Nav from './Nav';
 import MovieList from './MovieList';
 
 // Source: https://www.youtube.com/watch?v=35lXWvCuM8o
@@ -7,6 +8,7 @@ import MovieList from './MovieList';
 function App() {
   return (
     <div className='App'>
+      <Nav />
       <MovieList />
     </div>
   );
---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Navbar: https://www.w3schools.com/howto/howto_js_topnav.asp
+
+const Nav = () => {
+  return (
+    <div className='topnav'>
+      <li className='active'>MovieContext</li>
+      <li>Number of Movies: </li>
+    </div>
+  );
+};
+
+export default Nav;
