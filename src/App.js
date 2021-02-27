543657dfdccab8a922a0a6d2e918c9b431e5e3a3
ANS <36783010+answebdev@users.noreply.github.com>
2021-02-26 21:58:15 -0800

    add Movie component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import MovieList from './MovieList';
 
+// Source: https://www.youtube.com/watch?v=35lXWvCuM8o
+
 function App() {
   return (
     <div className='App'>
